529ec25dae1faca6be0bb019aa5b17f406f1a660
zeroalphat <taichi-takemura@cybozu.co.jp>
2025-03-03 13:57:25 +0900

    Create a template for the NodeImageSet CRD
    Signed-off-by: zeroalphat <taichi-takemura@cybozu.co.jp>

---
+++ api/v1/nodeimageset_types.go
@@ -0,0 +1,104 @@
+package v1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NodeImageSetSpec defines the desired state of NodeImageSet
+type NodeImageSetSpec struct {
+	// ImageSet is a list of image sets to be downloaded.
+	ImageSet []ImageSet `json:"imageSet"`
+
+	// NodeName is the name of the node where the image is downloaded.
+	NodeName string `json:"nodeName"`
+
+	// ImagePullSecrets is a list of secret names that contain credentials for authenticating with container registries
+	// +optional
+	ImagePullSecrets []corev1.LocalObjectReference `json:"imagePullSecrets,omitempty"`
+
+	// ImageDownloadRetryLimit specifies the maximum number of retry attempts for image downloads
+	// +optional
+	// +kubebuilder:default:=3
+	ImageDownloadRetryLimit int32 `json:"imageDownloadRetryLimit,omitempty"`
+}
+
+type ImageSet struct {
+	// image is the name of the image.
+	Image string `json:"image"`
+
+	// Registry Policy is the policy for downloading images from the registry.
+	RegistryPolicy RegistryPolicy `json:"registryPolicy"`
+}
+
+type RegistryPolicy string
+
+const (
+	// RegistryPolicyDefault download images according to containerd host configuration.
+	// If registry mirror are configured, it attempts to download images from registry mirror first,
+	// then falls back to upstream registry.
+	RegistryPolicyDefault RegistryPolicy = "Default"
+
+	// RegistryPolicyMirrorOnly downloads images only from registry mirrors defined in the containerd host configuration.
+	RegistryPolicyMirrorOnly RegistryPolicy = "MirrorOnly"
+)
+
+// NodeImageSetStatus defines the observed state of NodeImageSet
+type NodeImageSetStatus struct {
+	// The generation observed by the controller.
+	// +optional
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
+	// Conditions represent the latest available observations of an object's state
+	// +listType=map
+	// +listMapKey=type
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// DesiredImages is the number of images that need to be downloaded.
+	// +optional
+	//+kubebuilder:default:=0
+	DesiredImages int `json:"desiredImages,omitempty"`
+
+	// AvailableImages is the number of images that have completed downloading.
+	// +optional
+	//+kubebuilder:default:=0
+	AvailableImages int `json:"availableImages,omitempty"`
+
+	// DownloadFailedImages is the number of images that failed to download.
+	// +optional
+	//+kubebuilder:default:=0
+	DownloadFailedImages int `json:"downloadFailedImages,omitempty"`
+}
+
+const (
+	ConditionImageAvailable        = "ImageAvailable"
+	ConditionImageDownloadComplete = "ImageDownloadComplete"
+	ConditionImageDownloadFailed   = "ImageDownloadFailed"
+)
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster
+
+// NodeImageSet is the Schema for the nodeimagesets API
+type NodeImageSet struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NodeImageSetSpec   `json:"spec,omitempty"`
+	Status NodeImageSetStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// NodeImageSetList contains a list of NodeImageSet
+type NodeImageSetList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NodeImageSet `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&NodeImageSet{}, &NodeImageSetList{})
+}
---
+++ internal/controller/nodeimageset_controller_test.go
@@ -0,0 +1,77 @@
+package controller
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	ofenv1 "github.com/cybozu-go/ofen/api/v1"
+)
+
+var _ = Describe("NodeImageSet Controller", func() {
+	Context("When reconciling a resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		nodeimageset := &ofenv1.NodeImageSet{}
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind NodeImageSet")
+			err := k8sClient.Get(ctx, typeNamespacedName, nodeimageset)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &ofenv1.NodeImageSet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					Spec: ofenv1.NodeImageSetSpec{
+						ImageSet: []ofenv1.ImageSet{
+							{
+								Image:          "test-image",
+								RegistryPolicy: ofenv1.RegistryPolicyDefault,
+							},
+						},
+						NodeName: "test-node",
+					},
+					// TODO(user): Specify other spec details if needed.
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			// TODO(user): Cleanup logic after each test, like removing the resource instance.
+			resource := &ofenv1.NodeImageSet{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance NodeImageSet")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &NodeImageSetReconciler{
+				Client: k8sClient,
+				Scheme: k8sClient.Scheme(),
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+})
