b7b5ddfc234558adb747fa0b17e4a387c2f260e0
zeroalphat <taichi-takemura@cybozu.co.jp>
2025-03-05 11:22:14 +0900

    Create a template for the ImagePrefetch webhook
    Signed-off-by: zeroalphat <taichi-takemura@cybozu.co.jp>

---
+++ api/v1/imageprefetch_webhook_test.go
@@ -0,0 +1,31 @@
+package v1
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("ImagePrefetch Webhook", func() {
+
+	Context("When creating ImagePrefetch under Defaulting Webhook", func() {
+		It("Should fill in the default value if a required field is empty", func() {
+
+			// TODO(user): Add your logic here
+
+		})
+	})
+
+	Context("When creating ImagePrefetch under Validating Webhook", func() {
+		It("Should deny if a required field is empty", func() {
+
+			// TODO(user): Add your logic here
+
+		})
+
+		It("Should admit if all required fields are provided", func() {
+
+			// TODO(user): Add your logic here
+
+		})
+	})
+
+})
